d29145491b9dfe9226ea3581725a303a2f2b024f
Joel Jucá <joeljuca@proton.me>
2025-07-17 11:21:05 -0300

    refactor!: rewrite installation and GHA workflow

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for CHICKEN.
-GH_REPO="https://www.call-cc.org"
+GIT_REPO="https://code.call-cc.org/git/chicken-core.git"
 TOOL_NAME="chicken"
 TOOL_TEST="chicken -help"
 
@@ -14,26 +13,18 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if CHICKEN is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+list_git_tags() {
+	git ls-remote --tags --refs "$GIT_REPO" | grep -o 'refs/tags/.*' | cut -d/ -f3-
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
 	# Change this function if CHICKEN has other means of determining installable versions.
-	list_github_tags
+	list_git_tags
 }
 
 download_release() {
@@ -41,17 +32,16 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for CHICKEN
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="https://code.call-cc.org/releases/${version}/chicken-${version}.tar.gz"
 
-	echo "* Downloading $TOOL_NAME release $version..."
+	echo "* Downloading CHICKEN release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="${3%/bin}"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -59,12 +49,15 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+
+		cd $ASDF_DOWNLOAD_PATH
+		make PREFIX="${install_path}"
+		make PREFIX="${install_path}" install
 
 		# TODO: Assert CHICKEN executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
